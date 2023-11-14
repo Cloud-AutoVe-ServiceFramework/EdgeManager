2c02539937ed8e6bc11c1cd3bcc6dcc2b3696744
jeow7 <49180544+jeow7@users.noreply.github.com>
2023-11-14 10:35:06 +0900

    upload service files

---
+++ src/main/java/kr/re/etri/advcloud/service/EdgeMonitoringService.java
@@ -0,0 +1,41 @@
+package kr.re.etri.advcloud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.re.etri.advcloud.mapper.EdgeMonitoringMapper;
+import kr.re.etri.advcloud.model.EdgeMonitoringVO;
+
+@Service
+public class EdgeMonitoringService {
+	
+	@Autowired
+	private EdgeMonitoringMapper edgeMonitoringMapper;
+	
+	public int selectListCount (EdgeMonitoringVO edgeMonitoringVO) {
+		return edgeMonitoringMapper.selectListCount(edgeMonitoringVO);
+	}
+	
+	public List<EdgeMonitoringVO> selectList(EdgeMonitoringVO edgeMonitoringVO) {
+		return edgeMonitoringMapper.selectList(edgeMonitoringVO);
+	}
+
+	public EdgeMonitoringVO select(EdgeMonitoringVO edgeMonitoringVO) {
+		return edgeMonitoringMapper.select(edgeMonitoringVO);
+	}
+
+	public int insert(EdgeMonitoringVO edgeMonitoringVO) {
+		return edgeMonitoringMapper.insert(edgeMonitoringVO);
+	}
+
+	public int update(EdgeMonitoringVO edgeMonitoringVO) {
+		return edgeMonitoringMapper.update(edgeMonitoringVO);
+	}
+
+	public int delete(EdgeMonitoringVO edgeMonitoringVO) {
+		return edgeMonitoringMapper.delete(edgeMonitoringVO);
+	}
+	
+}

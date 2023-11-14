df2ac5b26ac51967195a781ab66d0b5b85cdb5f0
jeow7 <49180544+jeow7@users.noreply.github.com>
2023-11-14 10:34:10 +0900

    Upload mapper files

---
+++ src/main/java/kr/re/etri/advcloud/mapper/EdgeMonitoringMapper.java
@@ -0,0 +1,23 @@
+package kr.re.etri.advcloud.mapper;
+
+import java.util.List;
+
+import kr.re.etri.advcloud.common.annotation.Mapper;
+import kr.re.etri.advcloud.model.EdgeMonitoringVO;
+
+@Mapper("edgeMonitoringMapper")
+public interface EdgeMonitoringMapper {
+	
+	int selectListCount(EdgeMonitoringVO edgeMonitoringVO);
+	
+	List<EdgeMonitoringVO> selectList(EdgeMonitoringVO edgeMonitoringVO);
+
+	EdgeMonitoringVO select(EdgeMonitoringVO edgeMonitoringVO);
+
+	int update(EdgeMonitoringVO edgeMonitoringVO);
+
+	int insert(EdgeMonitoringVO edgeMonitoringVO);
+
+	int delete(EdgeMonitoringVO edgeMonitoringVO);
+
+}

c487498fb1d45dc1e67915e50b5205e7b8b0b271
jeow7 <49180544+jeow7@users.noreply.github.com>
2023-11-27 15:47:31 +0900

    update DateTimeUtil.java

--- src/main/java/kr/re/etri/advcloud/common/util/DateTimeUtil.java
+++ src/main/java/kr/re/etri/advcloud/common/util/DateTimeUtil.java
@@ -40,13 +40,13 @@ public class DateTimeUtil {
 		return uTCDateTime + timeZone.getOffset(uTCDateTime);
 	}
 
-	public static String getDateTimeFormatString(long dateTime) {
+	public static synchronized String getDateTimeFormatString(long dateTime) {
 		synchronized (syncLock) {
 			return simpleDateFormat.format(dateTime);
 		}
 	}
 
-	public static String getDateTimeFormatString(Date dateTime) {
+	public static synchronized String getDateTimeFormatString(Date dateTime) {
 		synchronized (syncLock) {
 			return simpleDateFormat.format(dateTime);
 		}
@@ -60,7 +60,7 @@ public class DateTimeUtil {
 	 * @param format  (ex. yyyy-MM-dd hh:mm:ss)
 	 * @return
 	 */
-	public static String getDateTimeFormatString(String strDate, int amount, String format) {
+	public static synchronized String getDateTimeFormatString(String strDate, int amount, String format) {
 		synchronized (syncLock) {
 			Calendar calendar = Calendar.getInstance();
 			Date date = calendar.getTime();
@@ -77,7 +77,7 @@ public class DateTimeUtil {
 				date = calendar.getTime();
 				strDate = sf.format(date);
 			} catch (ParseException e) {
-				logger.error(e.getMessage(), e); // XXX: 정적 검사 수정, 2022-10-11
+				logger.error(e.getMessage(), e); 
 			}
 
 			return strDate;
@@ -95,7 +95,7 @@ public class DateTimeUtil {
 
 			time = calendar.getTime().getTime();
 		} catch (ParseException e) {
-			logger.error(e.getMessage(), e); // XXX: 정적 검사 수정, 2022-10-11
+			logger.error(e.getMessage(), e); 
 		}
 
 		return time;
